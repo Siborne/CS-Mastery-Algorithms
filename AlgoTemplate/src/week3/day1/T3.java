56967d677fd224e8a1b8a67e1768e84e04abfaaf
Siborne <bo7785888@gmail.com>
2026-05-19 23:34:25 +0800

    Add RPN evaluator main to T3.java
    Implement main method in T3.java that reads a single line from stdin and evaluates a reverse Polish notation expression character-by-character using a Deque<Integer> (ArrayDeque). Supports single-digit operands and +, -, *, / operators, ignores '.' characters, stops on unrecognized characters, and prints the resulting value. Closes the Scanner after reading input.

--- AlgoTemplate/src/week3/day1/T3.java
+++ AlgoTemplate/src/week3/day1/T3.java
@@ -1,5 +1,44 @@
 package week3.day1;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class T3 {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+
+		String line = reader.nextLine();
+
+		Deque<Integer> stack = new ArrayDeque<>();
+		for (char c : line.toCharArray()) {
+			if (c == '.') {
+				continue;
+			} else if (c >= '0' && c <= '9') {
+				stack.push((int) (c - '0'));
+			} else if (c == '-') {
+				int num1 = stack.pop();
+				int num2 = stack.pop();
+				stack.push(num2 - num1);
+			} else if (c == '+') {
+				int num1 = stack.pop();
+				int num2 = stack.pop();
+				stack.push(num2 + num1);
+			} else if (c == '*') {
+				int num1 = stack.pop();
+				int num2 = stack.pop();
+				stack.push(num2 * num1);
+			} else if (c == '/') {
+				int num1 = stack.pop();
+				int num2 = stack.pop();
+				stack.push(num2 / num1);
+			} else {
+				break;
+			}
+		}
+		System.out.println(stack.pop());
+		reader.close();
+	}
 
 }
