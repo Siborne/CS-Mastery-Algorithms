96da3edf1809752cab65b2b078178c2879544534
Siborne <bo7785888@gmail.com>
2026-05-19 23:33:31 +0800

    Add week3/day1 templates and T1 solution
    Add four new files under AlgoTemplate/src/week3/day1. Implemented T1: reads integer n and a parentheses string, verifies balanced parentheses using a counter and prints "Yes" if balanced, otherwise "Nor". T2, T3 and T4 are added as empty class templates for future problems.

---
+++ AlgoTemplate/src/week3/day1/T1.java
@@ -0,0 +1,37 @@
+package week3.day1;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class T1 {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+
+		int n = reader.nextInt();
+		reader.nextLine();
+		String line = reader.nextLine();
+
+//		int[] arr = new int[n];
+		boolean flag = false;
+		int l = 0;
+		for (int i = 0; i < n; i++) {
+			char c = line.charAt(i);
+			if (c == '(') {
+				l++;
+			} else {
+				if (l > 0) {
+					l--;
+				} else {
+					flag = true;
+					break;
+				}
+			}
+		}
+
+		System.out.println(!flag && l == 0 ? "Yes" : "Nor");
+
+		reader.close();
+	}
+
+}
